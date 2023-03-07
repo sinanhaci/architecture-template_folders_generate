4bf9f88fe5925c5640a2094f2b01fecbfc3ecf0f
sinanhaci <sinan.hacia@gmail.com>
2023-03-07 11:20:11 +0300

    Added option with service.

--- extension.js
+++ extension.js
@@ -12,7 +12,18 @@ function activate(context) {
 			createMVVM(folderPath, folderName);
 		});
 	});
+	let disposableService = vscode.commands.registerCommand('sinanhacia.createMVVMWithService', function (uri) {
+		let folderPath = uri.fsPath;
+		vscode.window.showInputBox({
+			placeHolder: "Enter a name for the MVVM folders",
+			value: "enter_folder_name"
+		}).then(function (folderName) {
+			createMVVMWithService(folderPath, folderName);
+		});
+	});
 	context.subscriptions.push(disposable);
+	context.subscriptions.push(disposableService);
+
 }
 
 
@@ -27,6 +38,23 @@ function createMVVM(folderPath, folderName) {
 	fs.writeFileSync(path.join(subFolderPath, 'model', `${folderName}_model.dart`), generateModelClass(folderName));
 	fs.writeFileSync(path.join(subFolderPath, 'view', `${folderName}_view.dart`), generateVievClass(folderName));
 	fs.writeFileSync(path.join(subFolderPath, 'viewmodel', `${folderName}_viewmodel.dart`),  generateViewModelClass(folderName));
+}
+
+function createMVVMWithService(folderPath, folderName) {
+	// Create the MVVM folders
+	fs.mkdirSync(path.join(folderPath, folderName));
+	fs.mkdirSync(path.join(folderPath, folderName, 'model'));
+	fs.mkdirSync(path.join(folderPath, folderName, 'view'));
+	fs.mkdirSync(path.join(folderPath, folderName, 'viewmodel'));
+	fs.mkdirSync(path.join(folderPath, folderName, 'service'));
+
+	var subFolderPath = folderPath +"/"+ folderName + "/";
+	// Create the MVVM files
+	fs.writeFileSync(path.join(subFolderPath, 'model', `${folderName}_model.dart`), generateModelClass(folderName));
+	fs.writeFileSync(path.join(subFolderPath, 'view', `${folderName}_view.dart`), generateVievClass(folderName));
+	fs.writeFileSync(path.join(subFolderPath, 'viewmodel', `${folderName}_viewmodel.dart`),  generateViewModelClassWithService(folderName));
+	fs.writeFileSync(path.join(subFolderPath, 'service', `${folderName}_service.dart`),  generateServiceClass(folderName));
+
 }
 
 function generateVievClass (folderName){
@@ -85,6 +113,43 @@ abstract class _${toPascalCase(folderName)}ViewModelBase extends BaseRequestMeth
 
   @override
   void dispose() {}
+
+}`;
+}
+
+function generateViewModelClassWithService (folderName){
+	return `
+import 'package:mobx/mobx.dart';
+import '/export.dart';
+import '../service/${folderName}_service.dart';
+part '${folderName}_viewmodel.g.dart';
+
+class ${toPascalCase(folderName)}ViewModel = _${toPascalCase(folderName)}ViewModelBase with _$${toPascalCase(folderName)}ViewModel;
+
+abstract class _${toPascalCase(folderName)}ViewModelBase extends ${toPascalCase(folderName)}Service with Store, BaseViewModel {
+  
+  @override
+  void setContext(BuildContext context) {
+	context = context;
+	theme = Theme.of(context);
+	size = MediaQuery.of(context).size;
+  }
+
+  @override
+  void init() {}
+
+  @override
+  void dispose() {}
+
+}`;
+}
+
+function generateServiceClass (folderName){
+	return `
+import '/export.dart';
+
+class ${toPascalCase(folderName)}Service extends BaseRequestMethod {
+
 
 }`;
 }
